eb090ae176f12c8f656736310f8182b582cdafbf
yashodaAgarwal <55345522+yashodaAgarwal@users.noreply.github.com>
2021-11-17 18:10:41 +0530

    created index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your birthday is lucky 💥💥";
+    } else {
+        outputBox.innerText = "Your birthday is not lucky 😞";
+    }
+}
+
+
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum && dob)
+        compareValues(sum, luckyNumber.value)
+    else
+        outputBox.innerText = "Please enter both the fields.";
+}
+
+
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
